df0e60fced2cef3f8f615281536b543e8358a443
151porkyman <abradbury22@gmail.com>
2024-11-27 12:54:24 -0500

    Update wildbattle.asm

--- crysaudio/music/RBY/wildbattle.asm
+++ crysaudio/music/RBY/wildbattle.asm
@@ -1,14 +1,4 @@
 Music_WildBattle::
-	channel_count 1
-	channel 1, Music_WildBattle_Ch1
+	channel_count 0
 	
-Music_WildBattle_Ch1::
-	tempo 0
-	volume 0, 0
-	duty_cycle 3
-	vibrato 6, 3, 4
-	toggle_perfect_pitch
-	note_type 12, 11, 3
-	octave 4
-	note C_, 1
 	
