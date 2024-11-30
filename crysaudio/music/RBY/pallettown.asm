0473a488de1551605fb962774c315cf817d78140
151porkyman <abradbury22@gmail.com>
2024-11-29 20:12:32 -0500

    Update pallettown.asm
    Trying Ronald again

--- crysaudio/music/RBY/pallettown.asm
+++ crysaudio/music/RBY/pallettown.asm
@@ -6,284 +6,295 @@ Music_PalletTown:
 	channel 4, Music_PalletTown_Ch4
 
 Music_PalletTown_Ch1:
-    stereo_panning TRUE, TRUE
-    vibrato 12, 3, 3
-    duty_cycle 0
-    volume_envelope 9, 2
-MainLoop1
+	tempo 188
+	stereo_panning TRUE, TRUE
+	vibrato 12, 3, 3
+	duty_cycle 0
+	note_type 12, 12, 3
+
+MainLoop:
+	octave 3
+	volume_envelope 9, 2
+	note D_, 1
+	note D_, 1
+	note F_, 1
+	rest 2
+	note D_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note D_, 1
+	rest 1
+	note F_, 1
+	rest 1
+	volume_envelope 9, 0
+	note E_, 2
+	rest 1
+	octave 2
+	volume_envelope 9, 2
+	note G_, 1
+	octave 3
+	note D_, 1
+	note F_, 1
+	rest 2
+	note F_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note F_, 1
+	rest 1
+	note A_, 1
+	rest 1
+	volume_envelope 9, 0
+	note G_, 2
+	rest 1
+	sound_loop 2, MainLoop
+
+Branch_fa9aa:
+	vibrato 20, 3, 3
+	note C#, 3
+	note D_, 10
+	note D_, 2
+	vibrato 12, 3, 3
+	sound_loop 2, Branch_fa9aa
+
+Branch_fa9ec:
+	vibrato 20, 3, 3
+	note C#, 3
+	note D_, 10
+	note D_, 2
+	vibrato 12, 3, 3
+	sound_loop 2, Branch_fa9ec
+
+Branch_fa9cb:
+	vibrato 20, 3, 3
+	note C#, 3
+	note D_, 10
+	note D_, 2
+	vibrato 12, 3, 3
+	sound_loop 2, Branch_fa9cb
+
+MainLoop2:
+	volume_envelope 9, 0
+	rest 6
+	octave 2
+	note G_, 7
+	octave 3
+	note F_, 6
+	octave 2
+	note G_, 7
+	octave 3
+	note E_, 6
+	octave 2
+	note G_, 7
+	octave 3
+	note D_, 6
+	octave 2
+	note G_, 7
+	octave 3
+	note C_, 4
+	note C#, 4
+	note C_, 5
+	octave 2
+	note A#, 6
+	octave 3
+	note C_, 7
+	octave 2
+	note A#, 6
+	note G_, 7
+	note F_, 6
+	note F#, 7
+	note G_, 6
+	note A#, 7
+	octave 3
+	note C_, 6
+    note D_, 7
+    sound_call Branch_fa9aa
     octave 4
-    .Loop1
-        speed 1
-        note G_, 3
-        rest 3
-        note G_, 3
-        rest 4
-        note A#, 3
-        rest 10
-        inc_octave
-        note D_, 6
-        dec_octave
-        note G_, 3
-        rest 4
-        note A#, 3
-        rest 3
-        rest 13
-        note G_, 3
-        note D_, 4
-        note A#, 6
-        rest 7
-        note D_, 3
-        rest 3
-        note G_, 3
-        rest 4
-        note G_, 6
-        rest 7
-        rest 13
-        note A#, 6
-        rest 13
-        note G_, 3
-        note A#, 4
-        inc_octave
-        note D_, 6
-        rest 7
-        rest 13
-        speed 13
-        volume_envelope 9, 0
-        note C_, 2
-        rest 1
-        dec_octave
-        speed 1
-        volume_envelope 9, 2
-        note D_, 3
-        rest 3
-        note G_, 3
-        rest 4
-        note A#, 3
-        rest 10
-        inc_octave
-        note D_, 6
-        dec_octave
-        note G_, 3
-        rest 4
-        note A#, 3
-        rest 3
-        rest 13
-        note G_, 3
-        note A#, 4
-        inc_octave
-        note D_, 6
-        rest 7
-        dec_octave
-        note D_, 3
-        rest 3
-        note G_, 3
-        rest 4
-        note G_, 6
-        rest 7
-        rest 13
-        inc_octave
-        note D_, 6
-        rest 13
-        dec_octave
-        note A#, 3
-        inc_octave
-        note D_, 4
-        note F_, 6
-        rest 7
-        rest 13
-        speed 13
-        volume_envelope 9, 0
-        note E_, 2
-        rest 1
-        dec_octave
-        sound_call Branch_fa9aa, .Loop1
+    note C#, 3
+    note D_, 3
+    rest 7
+    duty_cycle 2
+    volume_envelope 9, 0
+    rest 6
+    octave 2
+    note G_, 7
+    octave 3
+    note F_, 6
+    octave 2
+    note G_, 7
+    octave 3
+    note E_, 6
+    octave 2
+    note G_, 7
+    octave 3
+    note D_, 6
+    octave 2
+    note G_, 7
+    octave 3
+    note C_, 4
+    note C#, 4
+    note C_, 5
+    octave 2
+    note A#, 6
+    octave 3
+    note C_, 7
+    octave 2
+    note A#, 6
+    note G_, 7
+    note F_, 6
+    note F#, 7
+    note G_, 6
+    note A#, 7
+    octave 3
+    note C_, 6
+    octave 2
+    rest 6
+    octave 2
+    note G_, 6
+    note D_, 7
+    rest 13
+    volume_envelope 10, 0
+    rest 13
+    octave 2
+    note G_, 3
+    note G#, 10
+    note G_, 3
+    rest 10
+    note F_, 4
+    volume_envelope 10, 1
+    rest 7
+    octave 2
+    note G_, 5
+    note F_, 5
 
 Music_PalletTown_Ch2:
-    stereo_panning TRUE, TRUE
-    vibrato 12, 3, 3
-    duty_cycle 0
-.MainLoop
-    octave 2
-.Loop1
-    speed 1
+	stereo_panning TRUE, TRUE
+	vibrato 12, 3, 3
+	duty_cycle 0
+	note_type 12, 13, 3
+
+MainLoop:
+    octave 3
     volume_envelope 9, 2
-    note G_, 3
-    rest 3
-    note G_, 3
-    rest 4
-    note A#, 3
-    rest 10
-    inc_octave
-    note D_, 6
-    dec_octave
-    note G_, 3
-    rest 4
-    note A#, 3
-    rest 3
-    rest 13
-    note G_, 3
-    note D_, 4
-    note A#, 6
-    rest 7
-    note D_, 3
-    rest 3
-    note G_, 3
-    rest 4
-    note G_, 6
-    rest 7
-    rest 13
-    note A#, 6
-    rest 13
-    note G_, 3
-    note A#, 4
-    inc_octave
-    note D_, 6
-    rest 7
-    rest 13
-    speed 13
-    volume_envelope 9, 0
-    note C_, 2
+    note D_, 1
+    note D_, 1
+    note F_, 1
+    rest 2
+    note D_, 1
     rest 1
-    dec_octave
-    speed 1
+    note C_, 1
+    rest 1
+    note D_, 1
+    rest 1
+    note F_, 1
+    rest 1
+    volume_envelope 9, 0
+    note E_, 2
+    rest 1
+    octave 2
     volume_envelope 9, 2
-    note D_, 3
-    rest 3
-    note G_, 3
-    rest 4
-    note A#, 3
-    rest 10
-    inc_octave
-    note D_, 6
-    dec_octave
-    note G_, 3
-    rest 4
-    note A#, 3
-    rest 3
-    rest 13
-    note G_, 3
-    note A#, 4
-    inc_octave
-    note D_, 6
-    rest 7
-    dec_octave
-    note D_, 3
-    rest 3
-    note G_, 3
-    rest 4
-    note G_, 6
-    rest 7
-    rest 13
-    inc_octave
-    note D_, 6
-    rest 13
-    dec_octave
+    note G_, 1
+    octave 3
+    note D_, 1
+    note F_, 1
+    rest 2
+    note F_, 1
+    rest 1
+    note C_, 1
+    rest 1
+    note F_, 1
+    rest 1
+    note A_, 1
+    rest 1
+    volume_envelope 9, 0
+    note G_, 2
+    rest 1
+    sound_loop 2, MainLoop
 
 Music_PalletTown_Ch3:
-    stereo_panning TRUE, TRUE
-    vibrato 12, 3, 3
-    duty_cycle 0
-    volume_envelope 9, 0
-.MainLoop
-    note C_, 6
+	stereo_panning TRUE, TRUE
+	vibrato 12, 3, 3
+	duty_cycle 0
+	note_type 12, 1, 2
+
+MainLoop:
+    octave 3
+    volume_envelope 9, 2
+    note D_, 1
+    note D_, 1
+    note F_, 1
+    rest 2
+    note D_, 1
     rest 1
-    note E_, 6
+    note C_, 1
     rest 1
-    note F_, 6
+    note D_, 1
     rest 1
-    note G_, 6
-    rest 1
-    note A_, 6
-    rest 1
-    note B_, 6
-    rest 1
-    note C_, 6
-    rest 1
-    note D_, 6
-    rest 1
-    note E_, 6
-    rest 1
-    note F_, 6
-    rest 1
-    note G_, 6
-    rest 1
-    note A_, 6
-    rest 1
-    note B_, 6
-    rest 1
-    note C_, 6
-    rest 1
-    note D_, 6
+    note F_, 1
     rest 1
     volume_envelope 9, 0
-    note F_, 6
+    note E_, 2
     rest 1
-    sound_call Branch_fa9aa, .MainLoop
+    octave 2
+    volume_envelope 9, 2
+    note G_, 1
+    octave 3
+    note D_, 1
+    note F_, 1
+    rest 2
+    note F_, 1
+    rest 1
+    note C_, 1
+    rest 1
+    note F_, 1
+    rest 1
+    note A_, 1
+    rest 1
+    volume_envelope 9, 0
+    note G_, 2
+    rest 1
+    sound_loop 2, MainLoop
 
 Music_PalletTown_Ch4:
-    stereo_panning TRUE, TRUE
-    vibrato 12, 3, 3
-    duty_cycle 0
-    volume_envelope 9, 0
-.MainLoop
-    octave 1
-    note G_, 3
-    rest 3
-    note G_, 3
-    rest 4
-    note A#, 3
-    rest 10
-    inc_octave
-    note D_, 6
-    dec_octave
-    note G_, 3
-    rest 4
-    note A#, 3
-    rest 3
-    rest 13
-    note G_, 3
-    note D_, 4
-    note A#, 6
-    rest 7
-    note D_, 3
-    rest 3
-    note G_, 3
-    rest 4
-    note G_, 6
-    rest 7
-    rest 13
-    note A#, 6
-    rest 13
-    note G_, 3
-    note A#, 4
-    inc_octave
-    note D_, 6
-    rest 7
-    rest 13
-    volume_envelope 9, 0
-    note C_, 2
-    rest 1
-    dec_octave
-    speed 1
+	toggle_noise 1
+	stereo_panning TRUE, TRUE
+	vibrato 12, 3, 3
+	duty_cycle 0
+MainLoop:
+    octave 3
     volume_envelope 9, 2
-    note D_, 3
-    rest 3
-    note G_, 3
-    rest 4
-    note A#, 3
-    rest 10
-    inc_octave
-    note D_, 6
-    dec_octave
-    note G_, 3
-    rest 4
-    note A#, 3
-    rest 3
-    rest 13
-    note G_, 3
-    note A#, 4
-    inc_octave
-    note D_, 6
-    rest 7
-    dec_octave
-    sound_call Branch_fa9aa, .MainLoop
+    note D_, 1
+    note D_, 1
+    note F_, 1
+    rest 2
+    note D_, 1
+    rest 1
+    note C_, 1
+    rest 1
+    note D_, 1
+    rest 1
+    note F_, 1
+    rest 1
+    volume_envelope 9, 0
+    note E_, 2
+    rest 1
+    octave 2
+    volume_envelope 9, 2
+    note G_, 1
+    octave 3
+    note D_, 1
+    note F_, 1
+    rest 2
+    note F_, 1
+    rest 1
+    note C_, 1
+    rest 1
+    note F_, 1
+    rest 1
+    note A_, 1
+    rest 1
+    volume_envelope 9, 0
+    note G_, 2
+    rest 1
+    sound_loop 2, MainLoop
