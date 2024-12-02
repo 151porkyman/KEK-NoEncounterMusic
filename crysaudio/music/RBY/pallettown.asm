b98f5f30d528627ce5eef8e6f5b42d9860011ffd
151porkyman <abradbury22@gmail.com>
2024-12-02 14:23:32 -0500

    Update pallettown.asm
    Trying Ronald again in the RBYPalletTown format, with 4 channels

--- crysaudio/music/RBY/pallettown.asm
+++ crysaudio/music/RBY/pallettown.asm
@@ -6,7 +6,7 @@ Music_RBYPalletTown::
 	channel 4, Music_RBYPalletTown_Ch4
 
 Music_RBYPalletTown_Ch1:
-	tempo 188
+	tempo 160
 	stereo_panning TRUE, TRUE
 	vibrato 12, 3, 3
 	duty_cycle 0
@@ -57,22 +57,6 @@ Branch_faGaa:
 	note D_, 2
 	vibrato 12, 3, 3
 	sound_loop 2, Branch_faGaa
-
-Branch_faGec:
-	vibrato 20, 3, 3
-	note C#, 3
-	note D_, 10
-	note D_, 2
-	vibrato 12, 3, 3
-	sound_loop 2, Branch_faGec
-
-Branch_faGcb:
-	vibrato 20, 3, 3
-	note C#, 3
-	note D_, 10
-	note D_, 2
-	vibrato 12, 3, 3
-	sound_loop 2, Branch_faGcb
 
 MainLoop2:
 	volume_envelope 9, 0
@@ -133,40 +117,40 @@ MainLoop2:
 	note G_, 7
 	octave 3
 	note C_, 4
-    note C#, 4
-    note C_, 5
-    octave 2
-    note A#, 6
-    octave 3
-    note C_, 7
-    octave 2
-    note A#, 6
-    note G_, 7
-    note F_, 6
-    note F#, 7
-    note G_, 6
-    note A#, 7
-    octave 3
-    note C_, 6
-    octave 2
-    rest 6
-    octave 2
-    note G_, 6
-    note D_, 7
-    rest 13
-    volume_envelope 10, 0
-    rest 13
-    octave 2
-    note G_, 3
-    note G#, 10
-    note G_, 3
-    rest 10
-    note F_, 4
-    volume_envelope 10, 1
-    rest 7
-    octave 2
-    note G_, 5
-    note F_, 5
+	note C#, 4
+	note C_, 5
+	octave 2
+	note A#, 6
+	octave 3
+	note C_, 7
+	octave 2
+	note A#, 6
+	note G_, 7
+	note F_, 6
+	note F#, 7
+	note G_, 6
+	note A#, 7
+	octave 3
+	note C_, 6
+	octave 2
+	rest 6
+	octave 2
+	note G_, 6
+	note D_, 7
+	rest 13
+	volume_envelope 10, 0
+	rest 13
+	octave 2
+	note G_, 3
+	note G#, 10
+	note G_, 3
+	rest 10
+	note F_, 4
+	volume_envelope 10, 1	
+	rest 7
+	octave 2
+	note G_, 5
+	note F_, 5
 
 Music_RBYPalletTown_Ch2:
 	stereo_panning TRUE, TRUE
@@ -175,42 +159,42 @@ Music_RBYPalletTown_Ch2:
 	note_type 12, 13, 3
 
 MainLoop:
-    octave 3
-    volume_envelope 9, 2
-    note D_, 1
-    note D_, 1
-    note F_, 1
-    rest 2
-    note D_, 1
-    rest 1
-    note C_, 1
-    rest 1
-    note D_, 1
-    rest 1
-    note F_, 1
-    rest 1
-    volume_envelope 9, 0
-    note E_, 2
-    rest 1
-    octave 2
-    volume_envelope 9, 2
-    note G_, 1
-    octave 3
-    note D_, 1
-    note F_, 1
-    rest 2
-    note F_, 1
-    rest 1
-    note C_, 1
-    rest 1
-    note F_, 1
-    rest 1
-    note A_, 1
-    rest 1
-    volume_envelope 9, 0
-    note G_, 2
-    rest 1
-    sound_loop 2, MainLoop
+	octave 3
+	volume_envelope 9, 2
+	note D_, 1
+	note D_, 1
+	note F_, 1
+	rest 2
+	note D_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note D_, 1
+	rest 1
+	note F_, 1
+	rest 1
+	volume_envelope 9, 0
+	note E_, 2
+	rest 1
+	octave 2
+	volume_envelope 9, 2
+	note G_, 1
+	octave 3
+	note D_, 1
+	note F_, 1
+	rest 2
+	note F_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note F_, 1
+	rest 1
+	note A_, 1
+	rest 1
+	volume_envelope 9, 0
+	note G_, 2
+	rest 1
+	sound_loop 2, MainLoop
 
 Music_RBYPalletTown_Ch3:
 	stereo_panning TRUE, TRUE
@@ -219,42 +203,42 @@ Music_RBYPalletTown_Ch3:
 	note_type 12, 1, 2
 
 MainLoop:
-    octave 3
-    volume_envelope 9, 2
-    note D_, 1
-    note D_, 1
-    note F_, 1
-    rest 2
-    note D_, 1
-    rest 1
-    note C_, 1
-    rest 1
-    note D_, 1
-    rest 1
-    note F_, 1
-    rest 1
-    volume_envelope 9, 0
-    note E_, 2
-    rest 1
-    octave 2
-    volume_envelope 9, 2
-    note G_, 1
-    octave 3
-    note D_, 1
-    note F_, 1
-    rest 2
-    note F_, 1
-    rest 1
-    note C_, 1
-    rest 1
-    note F_, 1
-    rest 1
-    note A_, 1
-    rest 1
-    volume_envelope 9, 0
-    note G_, 2
-    rest 1
-    sound_loop 2, MainLoop
+	octave 3
+	volume_envelope 9, 2
+	note D_, 1
+	note D_, 1
+	note F_, 1
+	rest 2
+	note D_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note D_, 1
+	rest 1
+	note F_, 1
+	rest 1
+	volume_envelope 9, 0
+	note E_, 2
+	rest 1
+	octave 2
+	volume_envelope 9, 2
+	note G_, 1
+	octave 3
+	note D_, 1
+	note F_, 1
+	rest 2
+	note F_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note F_, 1
+	rest 1
+	note A_, 1
+	rest 1
+	volume_envelope 9, 0
+	note G_, 2
+	rest 1
+	sound_loop 2, MainLoop
 
 Music_RBYPalletTown_Ch4:
 	toggle_noise 1
@@ -262,39 +246,39 @@ Music_RBYPalletTown_Ch4:
 	vibrato 12, 3, 3
 	duty_cycle 0
 MainLoop:
-    octave 3
-    volume_envelope 9, 2
-    note D_, 1
-    note D_, 1
-    note F_, 1
-    rest 2
-    note D_, 1
-    rest 1
-    note C_, 1
-    rest 1
-    note D_, 1
-    rest 1
-    note F_, 1
-    rest 1
-    volume_envelope 9, 0
-    note E_, 2
-    rest 1
-    octave 2
-    volume_envelope 9, 2
-    note G_, 1
-    octave 3
-    note D_, 1
-    note F_, 1
-    rest 2
-    note F_, 1
-    rest 1
-    note C_, 1
-    rest 1
-    note F_, 1
-    rest 1
-    note A_, 1
-    rest 1
-    volume_envelope 9, 0
-    note G_, 2
-    rest 1
-    sound_loop 2, MainLoop
+	octave 3
+	volume_envelope 9, 2
+	note D_, 1
+	note D_, 1
+	note F_, 1
+	rest 2
+	note D_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note D_, 1
+	rest 1
+	note F_, 1
+	rest 1
+	volume_envelope 9, 0
+	note E_, 2
+	rest 1
+	octave 2
+	volume_envelope 9, 2
+	note G_, 1
+	octave 3
+	note D_, 1
+	note F_, 1
+	rest 2
+	note F_, 1
+	rest 1
+	note C_, 1
+	rest 1
+	note F_, 1
+	rest 1
+	note A_, 1
+	rest 1
+	volume_envelope 9, 0
+	note G_, 2
+	rest 1
+	sound_loop 2, MainLoop
